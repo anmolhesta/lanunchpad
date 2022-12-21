88b300bb113d4c5108871a207e47e6e3267b50af
Anmol Hesta <anmol.hestbit@gmail.com>
2022-12-21 17:00:42 +0530

    added tables for user profile,added fileds in user table and package for user permissions

---
+++ database/migrations/2022_12_21_111747_create_user_profile_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('profile_picture',200);
+            $table->string('current_school_name',200)->nullable();;
+            $table->string('previous_school_name',200)->nullable();;
+            $table->string('parent_details',200)->nullable();;
+            $table->string('experience',10)->nullable();;
+            $table->string('expertise_subjects',500)->nullable();;
+            $table->integer('approved_by')->nullable();
+            $table->boolean('ref_status_id')->nullable();
+            $table->timestamp('verified_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile');
+    }
+}
