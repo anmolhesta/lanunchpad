fbe9ea4366d50286796e7be322b35d5cb3a912a6
Anmol Hesta <anmol.hestbit@gmail.com>
2022-12-21 19:56:13 +0530

    class seeders for profile data created

---
+++ database/seeders/UserProfileSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserProfile;
+use Illuminate\Database\Seeder;
+
+class UserProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        UserProfile::create([
+
+        ]);
+    }
+}
